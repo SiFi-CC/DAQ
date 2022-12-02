0c9ce2fd9b07a93ab3c894316306331b774081f9
Mark <mark@squarepotato.com>
2022-12-02 18:51:15 +0100

    MODULES to allow of configuration of slow control plots, snapshot interval includes also file with all data

--- monitoring/online.cxx
+++ monitoring/online.cxx
@@ -16,6 +16,7 @@
 #include <signal.h>
 
 #include "CommandLine.h"
+#include "Timer.h"
 volatile sig_atomic_t terminate;
 void signal_handler(int signum) {
     terminate = kTRUE;
@@ -63,18 +64,36 @@ void findFiberAddress(const char * f, std::vector<std::map<std::string, std::str
         }
     }
 }
-TGraph * CreateGraph(const char * name, const char *title) {
+TGraph * CreateGraph(std::string name, std::string title) {
     TGraph *g = new TGraph();
-    g->SetName(name);
-    g->SetTitle(title);
+    g->SetName(name.c_str() );
+    g->SetTitle(title.c_str() );
     g->GetXaxis()->SetTimeDisplay(1);
     g->GetXaxis()->SetTimeFormat("#splitline{%b %d}{%H:%M:%S}");
     g->GetYaxis()->SetMaxDigits(3);
     gDirectory->Add(g);
     return g;
 }
-void Process(const char *filename, ARGUMENTS arguments) {
-    TFile *fOutput = new TFile(filename, "RECREATE");
+void InitGraphs(std::map<std::string, TGraph *> &mapGraphs, nlohmann::json jModule) {
+    for (nlohmann::json::iterator it = jModule.begin(); it != jModule.end(); ++it) {
+        nlohmann::json obj = *it;
+        gDirectory->cd("/");
+        gDirectory->mkdir(Form("%s", obj["dir"].get<std::string>().c_str() ) );
+        gDirectory->cd(Form("/%s", obj["dir"].get<std::string>().c_str() ) );
+        for (nlohmann::json::iterator it = obj["contents"].begin(); it != obj["contents"].end(); ++it) {
+            nlohmann::json obj = *it;
+            mapGraphs[obj["name"].get<std::string>()] = CreateGraph(obj["name"].get<std::string>(), obj["title"].get<std::string>() );
+        }
+    }
+
+}
+void ParseModule(std::ifstream& in, nlohmann::json &j) {
+    std::ostringstream sstr;
+    sstr << in.rdbuf();
+    j = nlohmann::json::parse(sstr.str() );
+}
+void Process(ARGUMENTS arguments, nlohmann::json jModule) {
+    TFile *fOutput = new TFile("monitoring.root", "RECREATE");
     // frames and errors
     gDirectory->mkdir("frames");
     gDirectory->cd("/frames");
@@ -95,6 +114,7 @@ void Process(const char *filename, ARGUMENTS arguments) {
         vecTime.push_back(new TH1F(Form("hTime%d", i), Form("%d;t[ns]", i), 100, 0, 10000) ); //time 
     }
     findFiberAddress("../sandbox/sifi_params.txt", address);
+
     gDirectory->cd("/");
     gDirectory->mkdir("hits");
     gDirectory->cd("/hits");
@@ -102,29 +122,6 @@ void Process(const char *filename, ARGUMENTS arguments) {
     TH2S *hHitsR = new TH2S("hHitsR", "R;fiber;layer", 16, 0, 16, 4, 0, 4);
     hHitsL->SetOption("COLZ");
     hHitsR->SetOption("COLZ");
-    
-    gDirectory->cd("/");
-    gDirectory->mkdir("rshmp4040");
-    gDirectory->cd("/rshmp4040");
-    TGraph *gCh3_c = CreateGraph("gCh3_c", "Ch3 current; ;I[A]");
-    TGraph *gCh3_v = CreateGraph("gCh3_v", "Ch3 voltage; ;V[V]");
-    TGraph *gCh4_c = CreateGraph("gCh4_c", "Ch4 current; ;I[A]");
-    TGraph *gCh4_v = CreateGraph("gCh4_v", "Ch4 voltage; ;V[V]");
-    
-    gDirectory->cd("/");
-    gDirectory->mkdir("feba");
-    gDirectory->cd("/feba");
-    TGraph *gCh0_0_T = CreateGraph("gCh0_0_T", "FEB/A 0_0; ;T[^{o}C]");
-    TGraph *gCh0_1_T = CreateGraph("gCh0_1_T", "FEB/A 0_1; ;T[^{o}C]");
-    TGraph *gCh2_0_T = CreateGraph("gCh2_0_T", "FEB/A 2_0; ;T[^{o}C]");
-    TGraph *gCh2_1_T = CreateGraph("gCh2_1_T", "FEB/A 2_1; ;T[^{o}C]");
-
-    gDirectory->cd("/");
-    gDirectory->mkdir("localmachine");
-    gDirectory->cd("/localmachine");
-    TGraph *gDiskUsage = CreateGraph("gDiskUsage", "Disk usage; ;[%]");
-    TGraph *gIOReadCount = CreateGraph("gIOReadCount", "IO read count");
-    TGraph *gIOWriteCount = CreateGraph("gIOWriteCount", "IO write count");
 
     // connect to the zmq publisher created in the daq machine
     zmq::context_t ctx;
@@ -181,50 +178,54 @@ void Process(const char *filename, ARGUMENTS arguments) {
             vecTime[256 + i + 16*j]->Draw();
         }
     }
-    TCanvas *canHits = new TCanvas("canHits", "canHits");
-    canHits->Divide(1, 2);
-    canHits->cd(1);
+    TCanvas canHits("canHits", "canHits");
+    canHits.Divide(1, 2);
+    canHits.cd(1);
     hHitsL->Draw("COLZ");
-    canHits->cd(2);
+    canHits.cd(2);
     hHitsR->Draw("COLZ");
-
+    
+    // Slow Control TCanvas
+    std::map<std::string, TGraph *> mapGraphs;
+    InitGraphs(mapGraphs, jModule);
     TCanvas *canSlowControl = new TCanvas("canSlowControl", "canSlowControl");
     canSlowControl->Divide(4, 3);
-    canSlowControl->cd(1);
-    gCh3_v->Draw("ALP");
-    canSlowControl->cd(2);
-    gCh3_c->Draw("ALP");
-    canSlowControl->cd(3);
-    gCh4_v->Draw("ALP");
-    canSlowControl->cd(4);
-    gCh4_c->Draw("ALP");
-    canSlowControl->cd(5);
-    gCh0_0_T->Draw("ALP");
-    canSlowControl->cd(6);
-    gCh0_1_T->Draw("ALP");
-    canSlowControl->cd(7);
-    gCh2_0_T->Draw("ALP");
-    canSlowControl->cd(8);
-    gCh2_1_T->Draw("ALP");
-    canSlowControl->cd(9);
-    gDiskUsage->Draw("ALP");
-    canSlowControl->cd(10);
-    gIOReadCount->Draw("ALP");
-    canSlowControl->cd(11);
-    gIOWriteCount->Draw("ALP");
+    UShort_t counter = 1;
+    for(std::map<std::string, TGraph *>::iterator it = mapGraphs.begin(); it != mapGraphs.end(); ++it) {
+        canSlowControl->cd(counter);
+        it->second->Draw("ALP");
+        counter++;
+    }
 
     Double_t Tps = 1E12 / 200.e6;
     Float_t Tns = Tps / 1.e3;
-    const std::chrono::time_point before = std::chrono::steady_clock::now();
+
+    // Do stuff at every interval
+    Timer t = Timer();
+    Int_t lastN = 0;
+    t.setInterval([&]() {
+        //Copy contents from main TGraphs but in intervals, starting from the last read Nth entry
+        TFile *fInt = new TFile(Form("inter%d.root", lastN), "RECREATE");
+        std::map<std::string, TGraph *> _mapGraphs;
+        InitGraphs(_mapGraphs, jModule);
+        for(std::map<std::string, TGraph *>::iterator it = _mapGraphs.begin(); it != _mapGraphs.end(); ++it) {
+            for(UShort_t i=lastN; i < mapGraphs[it->first]->GetN(); ++i) {
+                _mapGraphs[it->first]->SetPoint(i-lastN, mapGraphs[it->first]->GetX()[i], mapGraphs[it->first]->GetY()[i]);
+            }
+        }
+        lastN = mapGraphs["gCh0_0_T"]->GetN();
+        if(fInt->Write() ) {
+            printf("Intermediate file %s written.\n", fInt->GetName() );
+        } else {
+            fprintf(stderr, "Error writing to intermediate file.\n");
+        }
+        fInt->Close();
+        delete fInt;
+
+    }, arguments.snapshot_interval);
 
     zmq::recv_result_t res = 0;
     while(!terminate) {
-        if (arguments.snapshot_type.compare("seconds") == 0 && arguments.snapshot_interval != 0) {
-            if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - before).count() > arguments.snapshot_interval) break;
-        }
-        if (arguments.snapshot_type.compare("events") == 0) {
-            // TODO:
-        }
         if (gSystem->ProcessEvents() ) break;
         zmq::message_t msg;
         try {
@@ -275,10 +276,10 @@ void Process(const char *filename, ARGUMENTS arguments) {
             } catch(nlohmann::json::parse_error &e) {
                 fprintf(stderr, "%d %s\n", e.id, e.what() );
             }
-            gCh3_c->AddPoint(j["timestamp"].get<double>(), j["ch3_c"].get<double>() );
-            gCh3_v->AddPoint(j["timestamp"].get<double>(), j["ch3_v"].get<double>() );
-            gCh4_c->AddPoint(j["timestamp"].get<double>(), j["ch4_c"].get<double>() );
-            gCh4_v->AddPoint(j["timestamp"].get<double>(), j["ch4_v"].get<double>() );
+            mapGraphs["gCh3_c"]->AddPoint(j["timestamp"].get<double>(), j["ch3_c"].get<double>() );
+            mapGraphs["gCh3_v"]->AddPoint(j["timestamp"].get<double>(), j["ch3_v"].get<double>() );
+            mapGraphs["gCh4_c"]->AddPoint(j["timestamp"].get<double>(), j["ch4_c"].get<double>() );
+            mapGraphs["gCh4_v"]->AddPoint(j["timestamp"].get<double>(), j["ch4_v"].get<double>() );
         }
         try {
             //message from the zmq publisher
@@ -294,10 +295,10 @@ void Process(const char *filename, ARGUMENTS arguments) {
             } catch(nlohmann::json::parse_error &e) {
                 fprintf(stderr, "%d %s\n", e.id, e.what() );
             }
-            gCh0_0_T->AddPoint(j["timestamp"].get<double>(), j["ch0_0_T"].get<double>() );
-            gCh0_1_T->AddPoint(j["timestamp"].get<double>(), j["ch0_1_T"].get<double>() );
-            gCh2_0_T->AddPoint(j["timestamp"].get<double>(), j["ch2_0_T"].get<double>() );
-            gCh2_1_T->AddPoint(j["timestamp"].get<double>(), j["ch2_1_T"].get<double>() );
+            mapGraphs["gCh0_0_T"]->AddPoint(j["timestamp"].get<double>(), j["ch0_0_T"].get<double>() );
+            mapGraphs["gCh0_1_T"]->AddPoint(j["timestamp"].get<double>(), j["ch0_1_T"].get<double>() );
+            mapGraphs["gCh2_0_T"]->AddPoint(j["timestamp"].get<double>(), j["ch2_0_T"].get<double>() );
+            mapGraphs["gCh2_1_T"]->AddPoint(j["timestamp"].get<double>(), j["ch2_1_T"].get<double>() );
         }
         try {
             //message from the zmq publisher
@@ -313,9 +314,9 @@ void Process(const char *filename, ARGUMENTS arguments) {
             } catch(nlohmann::json::parse_error &e) {
                 fprintf(stderr, "%d %s\n", e.id, e.what() );
             }
-            gDiskUsage->AddPoint(j["timestamp"].get<double>(), j["diskusage"].get<double>() );
-            gIOReadCount->AddPoint(j["timestamp"].get<double>(), j["iowritecount"].get<double>() );
-            gIOWriteCount->AddPoint(j["timestamp"].get<double>(), j["ioreadcount"].get<double>() );
+            mapGraphs["gDiskUsage"]->AddPoint(j["timestamp"].get<double>(), j["diskusage"].get<double>() );
+            mapGraphs["gIOReadCount"]->AddPoint(j["timestamp"].get<double>(), j["iowritecount"].get<double>() );
+            mapGraphs["gIOWriteCount"]->AddPoint(j["timestamp"].get<double>(), j["ioreadcount"].get<double>() );
         }
     }
     fOutput->Write();
@@ -325,8 +326,6 @@ void Process(const char *filename, ARGUMENTS arguments) {
     delete canQDCL;
     delete canTimeR;
     delete canTimeL;
-    delete canHits;
-    delete canSlowControl;
     delete server;
     delete fOutput;
 }
@@ -338,11 +337,10 @@ int main(int argc, char *argv[]) {
 
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
-    UShort_t counter = 0;
-    while(!terminate) {
-        std::string filename = "monitoring" + std::to_string(counter) + ".root";
-        Process(filename.c_str(), arguments);
-        counter++;
-    }
+    std::ifstream fModule;
+    fModule.open("MODULES");
+    nlohmann::json j;
+    ParseModule(fModule, j);
+    Process(arguments, j);
     return 0;
 }
